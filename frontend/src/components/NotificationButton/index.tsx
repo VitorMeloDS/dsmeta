fd4f612312fdec7d30bf717626b27995e80e9d99
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-07-12 20:36:21 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,12 +1,12 @@
-import icon from '../../assets/img/nofication-icon.svg'
-import './styles.css'
+import icon from "../../assets/img/nofication-icon.svg";
+import "./styles.css";
 
-function NotificationButton(){
+function NotificationButton() {
   return (
     <div className="dsmeta-red-btn">
       <img src={icon} alt="Notificar" />
     </div>
-  )
+  );
 }
 
-export default NotificationButton
+export default NotificationButton;
