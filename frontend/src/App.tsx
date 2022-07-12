23afe6b298426f28050775e0fc38628b838b9755
Vitor Melo da Silva <vmsvitor20@gmail.com>
2022-07-12 19:32:04 -0300

    First Component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
+import NotificationButton from "./components/NotificationButton"
 
 function App() {
   return (
-    <h1>Olá Mundo</h1>
+    <div>
+      <h1>Olá Mundo</h1>
+      <NotificationButton></NotificationButton>
+    </div>
   )
 }
 
